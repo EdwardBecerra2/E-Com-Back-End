541d083b32d9574b03e4561ee37a8f0ad7a37d6c
EdwardBecerra2 <146788503+EdwardBecerra2@users.noreply.github.com>
2024-02-29 13:18:49 -0800

    defined columns in productTag.js and tag.js... also imported modules in index.js

--- models/index.js
+++ models/index.js
@@ -5,12 +5,26 @@ const Tag = require('./tag');
 const ProductTag = require('./productTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'product_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
